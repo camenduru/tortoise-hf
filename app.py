77a61dbfd29bd2ff3d4322b12d24ad732465a4a8
Michael Nestor <mdnestor@users.noreply.huggingface.co>
2022-12-05 04:16:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+os.system("git clone https://github.com/jnordberg/tortoise-tts.git")
+os.system("pip install -r requirements.txt")
+os.system("python setup.py install")
+sys.path.append('./tortoise-tts/')
+
 import torch
 import torchaudio
 import torch.nn as nn
