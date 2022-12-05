cd28e4b9ae868138d74a77fe90124bcc5ad7cdba
Michael Nestor <mdnestor@users.noreply.huggingface.co>
2022-12-05 03:16:07 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+
+os.system("git clone https://github.com/jnordberg/tortoise-tts.git")
+os.system("cd tortoise-tts")
+os.system("pip install -r requirements.txt")
+os.system("python setup.py install")
+
+import torch
+import torchaudio
+import torch.nn as nn
+import torch.nn.functional as F
+
+from tortoise.api import TextToSpeech
+from tortoise.utils.audio import load_audio, load_voice, load_voices
+
+tts = TextToSpeech()
